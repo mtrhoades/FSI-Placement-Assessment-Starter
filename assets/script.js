ae22f1c322874680d17f28ba530149622a1d6c11
root <mtrhoades0727@gmail.com>
2021-11-22 20:04:20 -0800

    exam answers + more clean up

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,13 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!');
-
-// First, tell us your name
-let yourName = "Matthew Rhoades"; // HINT: Replace this with your own name!
+let yourName = "Matthew Rhoades"; 
 
 // We'll use these variables to track the counts of each cookie type
-// let gb = 0;      // Gingerbread
+
 
 let gb = localStorage.getItem('gb');
 if (gb === null) {
     gb = 0;
 } else {
-    gb = parseInt(gb)
+    gb = parseInt(gb);
 }
 document.querySelector("#qty-gb").textContent = gb;
 
@@ -19,7 +15,7 @@ let cc = localStorage.getItem('cc');
 if (cc === null) {
     cc = 0;
 } else {
-    cc = parseInt(cc)
+    cc = parseInt(cc);
 }
 document.querySelector("#qty-cc").textContent = cc;
 
@@ -28,28 +24,23 @@ let sugar = localStorage.getItem('sugar');
 if (sugar === null) {
     sugar = 0;
 } else {
-    sugar = parseInt(sugar)
+    sugar = parseInt(sugar);
 }
 document.querySelector("#qty-sugar").textContent = sugar;
 
 
-
-
-
 // Code to update name display 
+
 document.getElementById('credit').textContent = `Created by ${"Matthew Rhoades"}`;
 
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!');
     gb = gb + 1;
     document.querySelector("#qty-gb").textContent = gb;
     localStorage.setItem('gb', gb);
     document.querySelector("#qty-total").textContent = gb + cc + sugar
     
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
 });
 
 // TODO: Hook up event listeners for the rest of the buttons
@@ -73,7 +64,7 @@ document.getElementById('add-sugar').addEventListener('click', function() {
 });
 
 
-// adding minus buttons
+// adding minus buttons function
 
 document.getElementById('minus-gb').addEventListener('click', function() {
     console.log('Gingerbread + button was clicked!');
